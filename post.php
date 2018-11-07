3e95b932cc2411a784cefd029057d8c3428d8f0b
levisao <37148629+levisao@users.noreply.github.com>
2018-11-06 22:13:59 -0200

    Mudança no php e html
    Adição de Olá, "nome do usuário"

--- post.php
+++ post.php
@@ -1,6 +1,21 @@
 <?php 
 include_once "topo.php"; 
+
+session_start();
+
+// Está logado?
+  if ($_SESSION["logado"] == NULL) {
+      header("Location: login.php");
+  }
+
+
+$nome = $_SESSION["nome_usuario"];
+
+
 ?>
+
+
+<h1>Olá, <?php echo $nome  ?></h1>
 
 </body>
 </html>
