d9b324e51aa223f04a9e6ce7431bc4b8a62f4702
amandanfsoares <44984376+amandanfsoares@users.noreply.github.com>
2018-11-12 20:14:23 -0300

    Update topopost.php

--- topopost.php
+++ topopost.php
@@ -5,7 +5,7 @@
 		<link rel="shortcut icon"  a href="https://png.icons8.com/wired/2x/medical-thermometer.png" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-		
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" >
 	<style>
 		body{
 			 background-image: url("https://solucoesitapoasite.files.wordpress.com/2016/05/bg-azul-claro-degrade.jpg");
@@ -14,8 +14,9 @@
 			 background-size: 100% 100%;
 			 
 			}
-
-
+		
+	
+		}
 	</style>
 	</head>
 	<body>
